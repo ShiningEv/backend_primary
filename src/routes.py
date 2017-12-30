0bc408a1f7ed51f66a3203e12a8e28e3941ef141
Evan <xzy_1108@163.com>
2017-12-30 12:31:44 +0800

    add function redirect, response with 302.

--- src/routes.py
+++ src/routes.py
@@ -60,16 +60,29 @@ def route_index(request):
     return r.encode(encoding='utf-8')
 
 
-def response_with_headers(headers):
+def response_with_headers(headers, code=200):
     """
     Content-Type: text/html
     Set-Cookie: user=gua
     """
-    header = 'HTTP/1.1 210 VERY OK\r\n'
+    header = 'HTTP/1.1 {} VERY OK\r\n'.format(code)
     header += ''.join(['{}: {}\r\n'.format(k, v)
                        for k, v in headers.items()])
     return header
 
+def redirect(url):
+    """
+    浏览器在收到 302 响应的时候
+    会自动在 HTTP header 里面找 Location 字段并获取一个 url
+    然后自动请求新的 url
+    """
+    headers = {
+        'Location': url,
+    }
+    # 增加 Location 字段并生成 HTTP 响应返回
+    # 注意, 没有 HTTP body 部分
+    r = response_with_headers(headers, 302) + '\r\n'
+    return r.encode('utf-8')
 
 def route_login(request):
     """
@@ -136,7 +149,7 @@ def route_message(request):
     username = current_user(request)
     if username == '【游客】':
         log("**debug, route msg 未登录")
-        pass
+        return redirect('/')
     log('本次请求的 method', request.method)
     if request.method == 'POST':
         form = request.form()
